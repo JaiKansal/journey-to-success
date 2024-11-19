cf9e7a98c49dc2955320d8c5ef29186a7dd7df95
JaiKansal <147725965+JaiKansal@users.noreply.github.com>
2024-11-19 11:36:52 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/Motivation-and-To-Do' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/journey-to-success' : '',
 }
 
 module.exports = nextConfig 
